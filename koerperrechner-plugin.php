12cf6886aebe545907feed2331750749835802de
ScKoDEV <konstantinschiller@gmail.com>
2022-03-28 10:52:08 -0300

    Create Plugin Class

--- koerperrechner-plugin.php
+++ koerperrechner-plugin.php
@@ -28,3 +28,20 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
+
+if (! definded ('ABSPATH')) {
+    echo 'Can\t touch this!';
+    die;
+}
+
+class koerperrechnerPlugin
+{
+    function __construct($string) {
+        echo $string;
+    }
+}
+
+if ( class_exists('koerperrechnerPlugin')){
+$koerperrechnerPlugin = new koerperrechnerPlugin('Plugin initialized' ); 
+}
+
