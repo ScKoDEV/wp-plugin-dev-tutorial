70da4d4759a82a911b502dcc9dd216144a9500a4
ScKoDEV <konstantinschiller@gmail.com>
2022-03-28 10:30:22 -0300

    Create Plugin PHP file

---
+++ koerperrechner-plugin.php
@@ -0,0 +1,14 @@
+<?php 
+/**
+ * @package KoerperrechnerPlugin
+ */
+/*
+Plugin Name: Koerperrechner Plugin
+Plugin URI: https://plugin.schillerwebsolutions.com/plugin
+Description: This is my first attempt on writing a custom Plugin for this amazing tutorial series.
+Version: 1.0.0
+Author: Konstantin Schiller
+Author URI: https://plugin.schillerwebsolutions.com
+License: GPLv2 or later
+Text Domain: koerperrechner-plugin
+ */
