054cb784cbc8825e8af21dde97a85a7f77a8d46d
ScKoDEV <konstantinschiller@gmail.com>
2022-04-16 12:51:19 -0400

    Modular Admin SubPages

--- include/Pages/Admin.php
+++ include/Pages/Admin.php
@@ -4,40 +4,58 @@ namespace Inc\Pages;
 
 // need start with \ because we are not in the main directory we are in Parents
 use \Inc\Base\BaseController;
-use \Inc\SettingsAPI;
+use \Inc\API\SettingsAPI;
 
 class Admin extends BaseController
 {
 
     public $settings;
+    public $pages = array();
+    public $subpages = array();
+
     public function __construct(){
         $this->settings = new SettingsAPI();
-    }
-
-    public function register()
-    {
-       /* add_action( 'admin_menu',  array($this, 'add_admin_pages'));*/
-        $pages=[[
+        $this->pages = [
+            [
             'page_title' => 'Koerperrechner Plugin',
             'menu_title' => 'Koerperrechner',
             'capability' => 'manage_options',
             'menu_slug' => 'koerperrechner_plugin', 
-            'callback' => function(){echo'<h1>Alecadd Plugin</h1>';},
+            'callback' => function(){echo'<h1>SWS Plugin</h1>';},
              'icon_url' => 'dashicons-store',
              'position' => 110
-        ]];
-
-       $this->settings->addPages($pages)->register();
-        
+            ]
+    ];
+    $this->subpages = [
+        [
+            'parent_slug' => 'koerperrechner_plugin',
+            'page_title' => 'Custom Post Types',
+            'menu_title' => 'CPT',
+            'capability' => 'manage_options',
+            'menu_slug' => 'sws_cpt_plugin', 
+            'callback' => function(){echo'<h1>CPT Manager</h1>';}
+        ],
+        [
+            'parent_slug' => 'koerperrechner_plugin',
+            'page_title' => 'Custom Taxanomies',
+            'menu_title' => 'Taxonomies',
+            'capability' => 'manage_options',
+            'menu_slug' => 'sws_taxonomies_plugin', 
+            'callback' => function(){echo'<h1>Taxonomies Manager</h1>';}
+        ],
+        [
+            'parent_slug' => 'koerperrechner_plugin',
+            'page_title' => 'Custom Widgets',
+            'menu_title' => 'Widgets',
+            'capability' => 'manage_options',
+            'menu_slug' => 'sws_widgets_plugin', 
+            'callback' => function(){echo'<h1>Widgets Manager</h1>';}
+        ]
+];
     }
 
-    /* public function add_admin_pages(){
-        add_menu_page( 'Koerperrechner Plugin', 'Koerperrechner', 'manage_options', 'koerperrechner_plugin', array($this, 'admin_index'), 'dashicons-store', 110 );
-    
-    } 
-
-    public function admin_index() {
-        require_once $this->plugin_path . 'templates/admin.php';
+    public function register()
+    {
+       $this->settings->addPages( $this->pages )->withSubPage('Dashboard')->addSubPages($this->subpages)->register();
     }
-    */
 }
